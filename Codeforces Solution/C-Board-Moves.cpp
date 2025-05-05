d39745738fd6b5783e58a9c156ed4c022f0c35a9
Sumit Mishra <118822397+0ssm@users.noreply.github.com>
2025-05-05 07:05:07 +0530

    Update C-Board-Moves.cpp

--- Codeforces Solution/C-Board-Moves.cpp
+++ Codeforces Solution/C-Board-Moves.cpp
@@ -1,6 +1,25 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    cout<<"Hello world";
-    return 0;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    long long n;
+	    cin>>n;
+	    
+	    if(n == 1){
+	        cout<<0<<endl;
+	    } else{
+	    
+	    long long result = 0;
+	    for(long long i = 1; i <= n/2; ++i){
+	        result += 8 * i * i;
+	   }
+	   
+	    cout << result << endl;
+	    
+	        
+	    }
+	}   
 }
